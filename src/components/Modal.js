0c4a3803a8c7f0e9427e59493c644703dfa5a878
misharexviashvili <misharexviashvili@yahoo.com>
2023-05-06 14:32:19 +0400

    editing on double click works!

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,8 +14,9 @@ const validationSchema = Yup.object().shape({
     .min(5, "Phone number must be at list 5 digits long")
     .label("Phone Number"),
 });
-// TODO: in submit i should add axios.post to update server, also i should update UI with newly added user
-const Modal = ({ shown, hideModal }) => {
+
+const Modal = ({ shown, hideModal, rowData }) => {
+  console.log(rowData);
   return shown ? (
     <Formik
       initialValues={{
@@ -27,24 +28,43 @@ const Modal = ({ shown, hideModal }) => {
         phoneNumber: "",
       }}
       onSubmit={async (values) => {
-        // console.log(values);
-        try {
-          await axios.post("http://127.0.0.1:5000/api/data", {
-            id: Date.now(),
-            name: values.name,
-            email: values.email,
-            gender: values.gender,
-            address: {
-              street: values.street,
-              city: values.city,
-            },
-            phone: values.phoneNumber,
-          });
-          hideModal();
-          window.location.reload();
-        } catch (error) {
-          console.log(error);
-          alert("Something went wrong, please try again later");
+        if (rowData) {
+          try {
+            await axios.patch(`http://127.0.0.1:5000/api/data/${rowData.id}`, {
+              name: values.name,
+              email: values.email,
+              gender: values.gender,
+              address: {
+                street: values.street,
+                city: values.city,
+              },
+              phone: values.phoneNumber,
+            });
+            hideModal();
+            window.location.reload();
+          } catch (error) {
+            console.log(error);
+            alert("Something went wrong, please try again later");
+          }
+        } else {
+          try {
+            await axios.post("http://127.0.0.1:5000/api/data", {
+              id: Date.now(),
+              name: values.name,
+              email: values.email,
+              gender: values.gender,
+              address: {
+                street: values.street,
+                city: values.city,
+              },
+              phone: values.phoneNumber,
+            });
+            hideModal();
+            window.location.reload();
+          } catch (error) {
+            console.log(error);
+            alert("Something went wrong, please try again later");
+          }
         }
       }}
       validationSchema={validationSchema}
